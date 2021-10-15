f75170fbfecb83402eeb4e39c16845ac204c4784
LucaoMendes <lucas1999mendes@gmail.com>
2021-10-15 19:32:03 -0300

    Adicionada Snackbar com o retorno de cadastro

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,42 +1,64 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Image, Keyboard, Text, TouchableOpacity, TouchableOpacityBase, View } from "react-native";
 import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 import {
     TextInput,
-    HelperText,
+    Snackbar,
     Button
 } from 'react-native-paper'
 import styles from "./style";
 import {useFonts} from 'expo-font'
-import { loginUser } from "../../controller/usuarioController";
+import { loginUser , isLoggedIn, logoutUser } from "../../controller/usuarioController";
 
-export default function Login({navigation}){
+export default function Login({route , navigation}){
     const [usuario, setUsuario] = React.useState(null)
     const [senha, setSenha] = React.useState(null)
+    const [visibleSnack , setVisibleSnack] = React.useState(false)
+    const textSnack = "Cadastro efetuado com sucesso!"
+    const onToggleSnackBar = ()=> setVisibleSnack(!visibleSnack)
+    const onDismissSnackBar = () => setVisibleSnack(false)
+    
+    
+    useEffect(()=>{
+        if(route.params.cadastro)
+            onToggleSnackBar()
+    },[route])
+    
     let [fontsLoaded] = useFonts({
         'quicksand': require('../../assets/fonts/Quicksand-Light.ttf'),
       });
+
+
     function logar(){
         if(usuario == null || usuario=="" || senha == null || senha ==""){
             alert("Você precisa preencher todos os campos")
         }else{
-            if(loginUser(usuario,senha)){
-                navigation.replace("DrawerNavigation")
-            }
+            loginUser(usuario,senha,{navigation})
         }
     }
+
+    
     return(
         <TouchableWithoutFeedback onPress={()=>Keyboard.dismiss()} style={{height:'100%'}}>
+             
             <View style={styles.container}>
+            <Snackbar
+                visible={visibleSnack}
+                onDismiss={onDismissSnackBar}
+                style={{borderRadius:25,bottom:10}}
+                wrapperStyle={{alignContent:'center'}}
+                >
+                Cadastro concluido com sucesso!
+            </Snackbar>
                 <View style={styles.logoBox}>
-                <Image source={require('../../assets/logo.png')} style={styles.logo}/>
+                    <Image source={require('../../assets/logo.png')} style={styles.logo}/>
                 </View>
                 <View style={styles.inputBox}>
                     <TextInput 
                         mode='outlined'
                         keyboardType='email-address'
                         textContentType='emailAddress'
-                        label="Usuário"
+                        label="Email"
                         style={styles.authInputs}
                         onChangeText={usuario => setUsuario(usuario)}
                         value={usuario}/>
@@ -60,8 +82,10 @@ export default function Login({navigation}){
                         onPress={()=>logar()}>
                             Entrar
                         </Button>
+                        
                 </View>
             </View>
+           
         </TouchableWithoutFeedback>
     )
 
