e175caedb9edb024cc47339b1952294347ac7621
LucaoMendes <lucas1999mendes@gmail.com>
2021-11-29 12:21:44 -0300

    tela de adição de produtos

--- src/pages/addProdutos/index.js
+++ src/pages/addProdutos/index.js
@@ -1,9 +1,85 @@
 import React from "react";
+import {
+    TextInput,
+    HelperText,
+    Button
+} from 'react-native-paper'
+import { TouchableWithoutFeedback } from "react-native-gesture-handler";
+import styles from "./style";
+import { View , Keyboard, Image } from "react-native";
+import * as ImagePicker from 'expo-image-picker';
 
+export default function addProdutos (){ 
+    const [selectedImage, setSelectedImage] = React.useState(null);
+    const [produto, setProduto] = React.useState(null)
+    const [preco,setPreco] = React.useState(null)
 
-export default function addProdutos (){
+    let openImagePickerAsync = async () => {
+        let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    
+        if (permissionResult.granted === false) {
+          alert("Permission to access camera roll is required!");
+          return;
+        }
+    
+        let pickerResult = await ImagePicker.launchImageLibraryAsync();
+        if (pickerResult.cancelled === true) {
+            return;
+          }
+        setSelectedImage({ localUri: pickerResult.uri })
+        console.log(pickerResult);
+        console.log(selectedImage)
+      }
+
+      function GetImage(){
+          if(selectedImage == null){
+            return <Image style={styles.imgProduto} source={require('../../assets/imgs/sapatos/01.jpg')} />
+          }else{
+            return <Image style={styles.imgProduto} source={{
+                uri: selectedImage.localUri,
+                }} />
+          }
+      }
+    
     return (
-        <>
-        </>
+        <TouchableWithoutFeedback onPress={()=>Keyboard.dismiss()} style={{height:'100%'}}>
+            
+            <View style={styles.container}>
+                <View style={styles.boxImg}>
+                    <GetImage/>
+                    <View style={styles.btnGetImg}>
+                    <Button icon="archive-arrow-up" mode="contained" onPress={() =>{ openImagePickerAsync()}}>
+                        Escolher Arquivo
+                    </Button>
+                    </View>
+                </View>
+                <View style={styles.inputProd}>
+                    <View style={styles.nmProd}>
+                        <TextInput 
+                                    mode='outlined'
+                                    label="Nome do Produto"
+                                    style={styles.Inputs}
+                                    onChangeText={produto => setProduto(produto)}
+                                    value={produto}/>
+                    </View>
+                    <View style={styles.pcProd}>
+                        <TextInput 
+                                    mode='outlined'
+                                    label="Preço"
+                                    style={styles.Inputs}
+                                    onChangeText={preco => setPreco(preco)}
+                                    value={preco}
+                                    keyboardType="decimal-pad"
+                                />
+                    </View>
+                </View>
+                
+                <View style={styles.btnEnviar}>
+                <Button icon="content-save" mode="contained" onPress={() =>{ openImagePickerAsync()}}>
+                        Salvar Produto
+                    </Button>
+                </View>
+            </View>
+        </TouchableWithoutFeedback>
     )
 }
--- src/pages/addProdutos/style.js
+++ src/pages/addProdutos/style.js
@@ -1,6 +1,65 @@
 import { StyleSheet } from "react-native";
-
+import  colors  from '../../styles/colors'
 const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        flexDirection:"column",
+        height:'100%',
+        backgroundColor:colors.primaryBackgroundAppColor,
+    },
+    boxImg:{
+        height:"45%",
+        alignItems:'center'
 
+    },
+    imgProduto:{
+        marginTop:10,
+        height:"75%",
+        width:"80%",
+        backgroundColor:'black',
+        borderRadius:13,
+    },
+    btnGetImg:{
+        height:"15%",
+        justifyContent:'center',
+        margin:"2.5%",
+        width:"45%",
+    },
+    inputProd :{
+        marginHorizontal:10,
+        marginVertical:5,
+        justifyContent:'center',
+        height:75,
+        flexDirection:"row"
+    },
+    nmProd:{
+        width:"70%",
+        marginRight:"2.5%"
+    },
+    pcProd:{
+        width:"25%",
+        marginLeft:"2.5%"
+    },
+    btnEnviar : {
+        width:'45%',
+        alignSelf:'center',
+        marginHorizontal:10,
+        marginVertical:5,
+        justifyContent:'center',
+        height:75,
+    },Inputs:{
+        backgroundColor:colors.inputBackgroundColor,
+        height:55,
+        shadowColor:'#000',
+        shadowOffset:{
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.23,
+        shadowRadius: 2.62,
+        elevation:4,
+        
+        
+    },
 });
 export default styles
